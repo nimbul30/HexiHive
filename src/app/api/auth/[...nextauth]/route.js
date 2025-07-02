aced90124aaa0d9ef2912cfdd40ecdd3d4400662
Mark Fitchett <nimbul30@gmail.com>
2025-07-02 04:34:53 -0400

    Auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,19 +4,13 @@ import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
 import GoogleProvider from 'next-auth/providers/google';
 
-const spotifyScopes = [
-  'user-top-read',
-  'playlist-read-private',
-  'user-read-email',
-].join(',');
-
 const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization: `https://accounts.spotify.com/authorize?scope=${spotifyScopes}`,
+      authorization: `https://accounts.spotify.com/authorize?scope=user-top-read,playlist-read-private,user-read-email`,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -27,18 +21,19 @@ const handler = NextAuth({
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
-        token.provider = account.provider; // Add provider to the token
+        token.provider = account.provider;
       }
       return token;
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
-      session.provider = token.provider; // Expose provider to the session
+      session.provider = token.provider;
       return session;
     },
   },
   pages: {
     signIn: '/login',
+    error: '/auth/error',
   },
 });
 
